5db503b772dbf9751ed6b84f46332d5f2530ca2e
Djordje Dimitrov <110050027+djolodjolo23@users.noreply.github.com>
2023-06-15 16:12:04 +0200

    Working on the personal card, image added with Name and text card bellow it.

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,57 @@ void main() {
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Assignment 1',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const LayoutPage());
+  }
+}
+
+class LayoutPage extends StatelessWidget {
+  const LayoutPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Personal Card'),
+      ),
+      body: const Align(
+        alignment: Alignment.bottomCenter,
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image(
+                image: AssetImage('images/profile pic.png'),
+                width: 200.0,
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                'Djordje Dimitrov',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [Text('E-mail'), Text('Phone'), Text('Web:')],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
